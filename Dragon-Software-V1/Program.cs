43f835004275d0ea708dfc8ad130f892c1284754
Tobia <Tobia@LUFFES-LAPTOP>
2024-11-05 13:41:05 +0100

    Add project files.

---
+++ Dragon-Software-V1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dragon_Software_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm a dragon!");
+        }
+    }
+}
